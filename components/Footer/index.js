4b9b01c34c679276a43579f0c5bf5135a5badc2f
dnunes <douglas.silva@ulbra.inf.br>
2022-04-07 22:48:40 -0300

    init footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,6 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./styles.module.css";
+
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 };
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Litoral Saúde 2022 Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
