72f6fa17a6b9e476a30276d6e06a06095dc44e6a
dnunes <douglas.silva@ulbra.inf.br>
2022-04-10 21:34:50 -0300

    finish footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -12,7 +12,45 @@ const scrollToTop = () => {
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Litoral Saúde 2022 Todos os direitos reservados.</p>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <Image
+            className={styles.img}
+            width={121}
+            height={62}
+            src="/logo.svg"
+            alt="Logo da litoral saúde"
+          />
+          <address className={styles.infoContainer}>
+            <p className={styles.text}>
+              R. Garibaldi, 366 - Centro, Osório - RS, CEP: 95520-000
+            </p>
+            <div>
+              <a
+                className={styles.text}
+                href="mailto:atendimento@litoralsaude.com.br"
+              >
+                atendimento@litoralsaude.com.br
+              </a>
+              <a className={styles.text} href="tel:+555136010063">
+                +55 (51) 3601-0063
+              </a>
+            </div>
+          </address>
+          <button className={styles.button} onClick={scrollToTop}>
+            <Image
+              className={styles.icon}
+              width={28}
+              height={28}
+              src="/arrowUp.svg"
+              alt="Voltar para o topo dá página"
+            />
+          </button>
+        </div>
+      </div>
+      <p className={`${styles.cr} container-fluid`}>
+        Litoral Saúde 2022 Todos os direitos reservados.
+      </p>
     </footer>
   );
 };
