8331c82f44046fe131e6e3ba2c0818b11290e55b
dnunes <douglas.silva@ulbra.inf.br>
2022-04-14 14:35:55 -0300

    finish presentation

---
+++ components/HomePage/Presentation/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+import globalStyles from "../../../styles/global.module.css";
+import styles from "./styles.module.css";
+
+const Presentation = () => {
+  return (
+    <section className={styles.container}>
+      <div>
+        <p className={globalStyles.h3}>Osório -RS</p>
+        <h1 className={`${globalStyles.h12} ${styles.h1}`}>
+          Somos Litoral Saúde
+        </h1>
+        <p className={globalStyles.h4}>
+          Fornecedor de equipamentos médicos e odontológicos
+        </p>
+      </div>
+      <div className="d-none d-lg-block d-xl-block">
+        <Image
+          width={417}
+          height={415}
+          src="/home-img.png"
+          alt="Logo da litoral saúde"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Presentation;
