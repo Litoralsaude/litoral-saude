4c2e8e72840489de031ebef9a15eb4279e9363cc
dnunes <douglas.silva@ulbra.inf.br>
2022-04-16 19:11:21 -0300

    add active tab

--- components/Header/Navigation/index.js
+++ components/Header/Navigation/index.js
@@ -2,23 +2,32 @@ import React from "react";
 import Link from "next/link";
 import golbalStyles from "../../../styles/global.module.css";
 import headerStyles from "../styles.module.css";
+import { useRouter } from "next/router";
 
-const Li = ({ title, href }) => (
-  <li className={`${golbalStyles.h3} ${headerStyles.item}`}>
-    <Link href={href}>
-      <a>{title}</a>
-    </Link>
-  </li>
-);
+const Li = ({ title, href }) => {
+  const { pathname } = useRouter();
+
+  const isActive = pathname === href ? "active" : "";
+
+  return (
+    <li
+      className={`${golbalStyles.h3} ${headerStyles.item} ${headerStyles[isActive]}`}
+    >
+      <Link href={href}>
+        <a>{title}</a>
+      </Link>
+    </li>
+  );
+};
 
 const Navigation = ({ styles, navClassName }) => {
   return (
     <nav className={navClassName}>
       <ul className={styles.list}>
-        <Li title="Quem Somos" href={"/"} />
-        <Li title="Produtos" href={"/"} />
-        <Li title="Depoimentos" href={"/"} />
-        <Li title="Contato" href={"/"} />
+        <Li title="Quem Somos" href={"/quem-somos"} />
+        <Li title="Produtos" href={"/produtos"} />
+        <Li title="Depoimentos" href={"/depoimentos"} />
+        <Li title="Contato" href={"/contato"} />
       </ul>
     </nav>
   );
