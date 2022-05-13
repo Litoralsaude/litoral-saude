a3268da830af09304456157af3dd385932ac755d
dnunes <douglas.silva@ulbra.inf.br>
2022-05-13 18:36:09 -0300

    add new testimonial

--- components/Testimonials/data.js
+++ components/Testimonials/data.js
@@ -1,9 +1,9 @@
 export const testimonials = [
   {
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lacinia sodales justo, id consectetur turpis cursus eget. cursus mattis velit.",
-    user: "Pedro Silva, Médico em Porto Alegre.",
-    photo: "/osorio.jpg",
+      "Sou cliente há mais de 10 anos, sempre fui muito bem atendida. Ótimo custo x benefício.",
+    user: "Rovene - Clínica Lótus",
+    photo: "/lotus-logo.jpeg",
   },
   {
     description:
